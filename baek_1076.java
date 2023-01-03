322bc8f7c83bacc4b605c61f038711393f14c4bd
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-01-03 11:08:38 +0900

    new BJ
    new BJ

---
+++ baek_1076.java
@@ -0,0 +1,20 @@
+//package BaekJoon;
+//
+//import java.util.Scanner;
+//
+//public class baek_1076 {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        String color1 = scan.next();
+//        String color2 = scan.next();
+//        String coolor3 = scan.next();
+//
+//        int[][] array = {{0,1},{1,10},{2,100},
+//                {3,1000},{4,10000},
+//                {5,100000},{6,1000000},
+//                {7,10000000},{8,100000000},
+//                {9,1000000000}};
+//
+//
+//    }
+//}
