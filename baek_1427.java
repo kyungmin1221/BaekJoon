5131f0234a05eeb134b27a5a50c25b16f18162ed
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-01-13 17:57:45 +0900

    0113
    0113

---
+++ baek_1427.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class baek_1427 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        int[] A = new int[str.length()];
+        for(int i=0; i<str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i,i+1));     // 2 1 4 3 하나씩 배열에 삽입
+        }
+        for(int i=0; i<str.length(); i++) {
+            int MAX = i;                                     // 현재위치를 최대값으로 일단 지정
+            for(int j=i+1; j<str.length(); j++) {
+                if(A[j] > A[MAX]) {
+                    MAX =j;
+                }
+            }
+            if(A[i] < A[MAX]) {        // 찾은 최댓값과 자리를 바꾸어 주는 작업
+                int temp = A[i];
+                A[i] = A[MAX];
+                A[MAX] = temp;
+            }
+        }
+        for(int i=0; i<str.length(); i++) {
+            System.out.print(A[i]);
+        }
+    }
+}
---
+++ baek_2164.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class baek_2164 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();    // 카드 개수 입력
+        Queue<Integer> que = new LinkedList<Integer>();   // 큐 생성
+        for(int i=1; i<= N; i++)
+        {
+            que.add(i);
+        }
+        while(que.size() > 1) {        // 카드가 마지막 한장이 남을 때 까지
+            que.poll();               // 맨위 카드 버림
+            que.add(que.poll());     // 맨위 카드 버린걸 뒤에 추가 (핵심)
+         }
+        System.out.println(que.poll());
+    }
+}
