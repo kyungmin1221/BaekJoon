e316c41871d7ca101c9cf9e4b85855600f4a548e
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-01-15 21:40:45 +0900

    0115
    0115

--- baek_2750.java
+++ baek_2750.java
@@ -6,12 +6,12 @@ public class baek_2750 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        int A[] = new int[N];   // 크기가 N 인 배열 생성
+        int A[] = new int[N];   // 크기가 N 인 배열 생성  = 5
         for(int i=0; i<N; i++) {
             A[i] = scanner.nextInt();
         }
-        for(int i=0; i<N-1; i++) {
-            for (int j = 0; j < N - 1 - i; j++) {
+        for(int i=0; i<N-1; i++) {            // 0 ~ 4
+            for (int j = 0; j < N - 1 - i; j++) {       // 0 ~ 4-i
                 if (A[j] > A[j + 1]) {
                     int temp = A[j];
                     A[j] = A[j + 1];
