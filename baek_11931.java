af6064ddcd96d55589decbff353aa5b15d6ef79e
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-01-15 22:02:31 +0900

    0115
    0115

---
+++ baek_11931.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class baek_11931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(bf.readLine());
+        Integer[] A = new Integer[N];
+        for(int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(bf.readLine());
+        }
+        Arrays.sort(A, Collections.reverseOrder());
+        for (int i=0; i<N; i++) {
+            sb.append(A[i]).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+}
---
+++ baek_2751.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class baek_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder st = new StringBuilder();
+        int N = Integer.parseInt(bf.readLine());
+        int[] A = new int[N];
+        for(int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(bf.readLine());
+        }
+        Arrays.sort(A);
+
+        for(int i=0; i<N; i++) {
+            st.append(A[i]).append('\n');
+        }
+        System.out.println(st);
+
+    }
+}
