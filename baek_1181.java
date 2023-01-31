0c0ce3a86292624e8d61334bed9f60fe133ef66f
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-01-31 19:59:19 +0900

    0131
    0131

---
+++ baek_1181.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baek_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        StringBuilder st = new StringBuilder();
+        String[] A = new String[N];
+        for(int i =0; i<N; i++) {
+            A[i] = String.valueOf(Integer.parseInt(st.toString()));
+        }
+    }
+}
