faad8e850a43d3a8a7caac1bc2f3e5618a22554d
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-08-08 15:49:58 +0900

    K 번째 수 (퀵정렬)

---
+++ Baek_11004.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek_11004 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        st = new StringTokenizer(bf.readLine());
+        for(int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        
+
+
+    }
+}
