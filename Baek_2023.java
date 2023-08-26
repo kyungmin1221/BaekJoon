35015c6c0709c01da22b7d2e28ce0378c63b7b3a
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-08-26 15:11:45 +0900

    신기한 소수(DFS)

---
+++ Baek_2023.java
@@ -0,0 +1,50 @@
+package BaekJoon;
+
+// 신기한 소수 //
+// DFS
+// 재귀함수
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_2023 {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        DFS(2, 1);
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
+    }
+
+    static void DFS(int number, int jarisu) {
+        if (jarisu == N) {
+            if (isPrime(number)) {
+                System.out.println(number);
+            }
+            return;
+        }
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (isPrime(number * 10 + i)) {
+                DFS(number * 10 + i, jarisu + 1);
+            }
+        }
+    }
+
+    static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
