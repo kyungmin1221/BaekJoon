8d843cb02fab7b65f72ed4cd10fe0118e374b09b
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-08-29 14:35:36 +0900

    수 찾기(이진,이분탐색)

--- Baek_1920.java
+++ Baek_1920.java
@@ -2,7 +2,7 @@ package BaekJoon;
 
 // 수 찾기 //
 
-// 이진탐색
+// 이진탐색(이분탐색)
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,41 +12,46 @@ import java.util.StringTokenizer;
 
 public class Baek_1920 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] A = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for(int i=0; i<N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
-        // 이진탐색을 위해 오름차순 정렬
+        // 오름차순 정렬
+        // 1 2 3 4 5
+        // 그리고 찾으려하는 배열과 비교
         Arrays.sort(A);
 
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<m; i++) {
+        int M = Integer.parseInt(bf.readLine());
+        st = new StringTokenizer(bf.readLine());
+        
+        for(int i=0; i<M; i++) {
             boolean answer = false;
-            int target = Integer.parseInt(st.nextToken());
             int start = 0;
             int end = A.length - 1;
+            int target = Integer.parseInt(st.nextToken());
+
             while (start <= end) {
                 int mid = (start + end) / 2;
-                int midv = A[mid];
-                if(midv>target) {
-                    end = mid-1;
-                } else if (midv<target) {
-                    start = mid+1;
-                }else {
+                int midValue = A[mid];
+                if (midValue > target) {
+                    end = mid - 1;
+                } else if (midValue < target) {
+                    start = mid + 1;
+                } else {
                     answer = true;
                     break;
                 }
             }
-            if(answer) {
-                System.out.println(1);
+
+            if (answer) {
+                System.out.println("1");
             } else {
-                System.out.println(0);
+                System.out.println("0");
             }
         }
+
     }
 }
