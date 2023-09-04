dc3a391510105a402dfaaeeb7f68207384d77e21
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-09-04 20:44:18 +0900

    DFS 와 BFS

--- Baek_1260.java
+++ Baek_1260.java
@@ -8,7 +8,6 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -17,23 +16,51 @@ public class Baek_1260 {
     static int N,E,S;
     static int[][] Graph;
     static boolean[] Visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
 
+        // 그래프와 방문 배열 초기화
+        // 0번 인덱스를 비워놓는 이유는 주로 노드 번호가 1부터 시작할 경우, 배열의 인덱스와 노드 번호를 일치시키기 위해서
+        Graph = new int[N+1][N+1];
+        Visited = new boolean[N+1];
+
+        for(int i=0; i<E; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            // 양방향 엣지를 표현
+            Graph[u][v] = Graph[v][u] = 1;
+        }
+        Visited = new boolean[N+1];
+        // 함수 호출
+        DFS(Graph,Visited,S);
+        System.out.println(sb);
+
+        Visited = new boolean[N+1];
+        BFS(Graph,Visited,S);
+    }
     static void DFS(int[][] Graph, boolean[] Visited, int S) {
-        StringBuilder sb = new StringBuilder();
-         Visited[S] = true;
-         sb.append(S).append(" ");
+        // StringBuilder sb = new StringBuilder();
+        Visited[S] = true;
+        sb.append(S).append(" ");
 
-         for(int i=1; i<Graph[S].length; i++) {
-             if(Graph[S][i] == 0) {
-                 continue;
-             }
-             int next = i;
-             if(!Visited[next]) {
-                 Visited[next] = true;
-                 DFS(Graph,Visited,next);
-             }
-         }
-        System.out.println("DFS: " + sb);
+        for(int i=1; i<Graph[S].length; i++) {
+            if(Graph[S][i] == 0) {
+                continue;
+            }
+            int next = i;
+            if(!Visited[next]) {
+                Visited[next] = true;
+                DFS(Graph,Visited,next);
+            }
+        }
+//        System.out.println("DFS: " + sb);
     }
     static void BFS(int[][] Graph, boolean[] Visited, int S) {
         // BFS는 큐(선입선출)로 구현할 수 있다.
@@ -67,31 +94,6 @@ public class Baek_1260 {
             }
         }
         // 최종 출력
-        System.out.println("BFS : " + sb);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-
-        // 그래프와 방문 배열 초기화
-        // 0번 인덱스를 비워놓는 이유는 주로 노드 번호가 1부터 시작할 경우, 배열의 인덱스와 노드 번호를 일치시키기 위해서
-        Graph = new int[N+1][N+1];
-        Visited = new boolean[N+1];
-
-        for(int i=0; i<E; i++) {
-            st = new StringTokenizer(bf.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            // 양방향 엣지를 표현
-            Graph[u][v] = Graph[v][u] = 1;
-        }
-        // 함수 호출
-        DFS(Graph,Visited,S);
-        BFS(Graph,Visited,S);
+        System.out.println(sb);
     }
 }
