9b7bb33474f7a65839c0b1df6a0bf0bb85606770
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-09-19 14:54:14 +0900

    섬의 개수(DFS,BFS)

--- silver/Baek_4963.java
+++ silver/Baek_4963.java
@@ -17,32 +17,35 @@ public class Baek_4963 {
     static int[] dx = {0, 1, 0, -1, 1, 1, -1, -1};
     static int[] dy = {1, 0, -1, 0, 1, -1, 1, -1};
 
-    static int N,M;
+    static int w,h;
     static boolean[][] Visited;
     static int[][] A;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        A = new int[M][N];
-        Visited = new boolean[M][N];
 
-        for(int i = 0; i < M; i++){
-            st = new StringTokenizer(bf.readLine());
-            for(int j = 0; j < N; j++){
-                A[i][j] = Integer.parseInt(st.nextToken());
+        while (w != 0 && h != 0) {                  // 입력의 마지막이 0 0 이면 종료
+            w = Integer.parseInt(st.nextToken());   // 5
+            h = Integer.parseInt(st.nextToken());   // 4
+            A = new int[h][w];                      // 4 x 5 크기의 배열
+            Visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(bf.readLine());
+                for (int j = 0; j < w; j++) {
+                    A[i][j] = Integer.parseInt(st.nextToken());
+                }
             }
-        }
 
-        land(0,0);
+            land(0, 0);
+            System.out.println(A[h - 1][w - 1]);
+        }
     }
 
-    public static int land(int start, int end) {
+    public static void land(int start, int end) {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[] {start,end});
         Visited[start][end] = true;
-        int answer = 0; // 섬의 개수를 저장할 변수
         while(!queue.isEmpty()) {
             int now[] = queue.poll();   // 처음은 (0,0)
             for(int k=0; k<8; k++) {
@@ -54,8 +57,7 @@ public class Baek_4963 {
                     queue.add(new int[] {x,y});
                 }
             }
-            answer = answer + 1;
         }
-        return answer;
+
     }
 }
