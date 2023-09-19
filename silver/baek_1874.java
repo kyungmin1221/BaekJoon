b9a73fba576022411ceac224c898c537fd990a91
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-09-19 14:51:26 +0900

    스택수열( Stack )

--- silver/baek_1874.java
+++ silver/baek_1874.java
@@ -1,11 +1,12 @@
-package BaekJoon;
+package BaekJoon.silver;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-
+//
 public class baek_1874 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
