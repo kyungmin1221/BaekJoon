84020f8c129bc9b5cd0b79a409e74777b91e95e1
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-11-13 00:07:23 +0900

    프린터 큐(시뮬레이션&Queue)

--- silver/Baek_1966.java
+++ silver/Baek_1966.java
@@ -7,31 +7,56 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+
+// 프린터 큐 //
+// 시뮬레이션 , 큐
 public class Baek_1966 {
     static int T,N,M;	// 테스트 케이스,문서의 개수,중요
-    static int priary;
 
-    static void solution(int N, int M, int priary) {
-
-
-    }
 
     public static void main(String args[]) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        Queue<Integer> queue = new LinkedList<>();
+
         T = Integer.parseInt(bf.readLine());
-        int testcase = 1;
-      //   while(testcase<=T) {
-             StringTokenizer st = new StringTokenizer(bf.readLine());
-             N = Integer.parseInt(st.nextToken());
-             M = Integer.parseInt(st.nextToken());
 
-             st = new StringTokenizer(bf.readLine());
-             for (int i = 0; i < N; i++) {
-                 queue.offer(Integer.parseInt(st.nextToken()));
-             }
+        for(int t=0; t<T; t++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
 
-             solution(N, M, priary);
-         }
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            Queue<int[]> queue = new LinkedList<>();
+            int count = 0;
+            st = new StringTokenizer(bf.readLine());
+            for (int i = 0; i < N; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                queue.offer(new int[]{i, num});
+            }
 
+            while(true) {
+                int[] cur = queue.poll();
+                boolean check = true;
+
+                for(int[] primary : queue) {
+                    // poll() 한 원소의 중요도[1]와 현재 큐의 첫번째 원소의 중요도[1] 을 비교
+                    if(primary[1]>cur[1]) {     // 현재 큐의 첫번째 중요도가 더 크다면 방금 poll() 한 원소를 뒤로 보내야됌
+                        check = false;
+                        break;
+                    }
+                }
+                // poll() 한 원소의 중요도가 제일 높다면
+                if(check) {
+                    count++; // 인쇄 시작
+                    // 꺼낸 원소의 인덱스 번호가 원하는 인덱스와 같다면
+                    if(cur[0] == M) {
+                        break;
+                    }
+                } else {
+                    queue.offer(cur);
+                }
+
+            }
+            System.out.println(count);
+        }
+    }
 }
+
