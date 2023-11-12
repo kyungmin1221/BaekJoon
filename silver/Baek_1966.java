9e586a0327844ac5aab2a8af388b03fa8d8ef420
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-11-12 15:17:28 +0900

    프린트 큐(시뮬레이션)

--- silver/Baek_1966.java
+++ silver/Baek_1966.java
@@ -9,43 +9,29 @@ import java.util.StringTokenizer;
 
 public class Baek_1966 {
     static int T,N,M;	// 테스트 케이스,문서의 개수,중요
-    static String priary;
+    static int priary;
 
-    static void solution(int N, int M, String priary) {
-        Queue<Character> queue = new LinkedList<>();
-        char[] num = priary.toCharArray();
-        int copy = 0;
+    static void solution(int N, int M, int priary) {
 
-        for(int i=0; i<N; i++) {
-            queue.offer(num[i]);
-        }
-        while(!queue.isEmpty()) {
-            char a = queue.poll();
-            char b = queue.poll();
-            // 현재 문서보다 중요도가 높은 문서가 있다면 큐에 뒤에 재배치 , 아니면 인쇄
-            if(a<b) {
-                queue.offer(a);
-            } else
-        }
 
     }
 
     public static void main(String args[]) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> queue = new LinkedList<>();
+        T = Integer.parseInt(bf.readLine());
+        int testcase = 1;
+      //   while(testcase<=T) {
+             StringTokenizer st = new StringTokenizer(bf.readLine());
+             N = Integer.parseInt(st.nextToken());
+             M = Integer.parseInt(st.nextToken());
 
-         T = Integer.parseInt(bf.readLine());
-         int testcase = 1;
-        // while(testcase<=T) { }
-        StringTokenizer st  = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M= Integer.parseInt(st.nextToken());
+             st = new StringTokenizer(bf.readLine());
+             for (int i = 0; i < N; i++) {
+                 queue.offer(Integer.parseInt(st.nextToken()));
+             }
 
-        st  = new StringTokenizer(bf.readLine());
-        for(int i=0; i<N; i++) {
-            priary = st.nextToken();
-        }
+             solution(N, M, priary);
+         }
 
-        solution(N,M,priary);
-
-    }
 }
