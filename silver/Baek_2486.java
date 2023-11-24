820579164a5a4a9c3db78180e4c3983f91dbb398
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-11-24 23:28:07 +0900

    안전영역(그래프탐색)

--- silver/Baek_2486.java
+++ silver/Baek_2486.java
@@ -6,51 +6,37 @@ package BaekJoon.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Baek_2486 {
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int N;
     static int[][] map;
     static boolean[][] visited;
+    static int safelevel = 0;
+    static int maxcount = Integer.MIN_VALUE;
 
-    static int safezone = 0;
-    static void safe(int x, int y,int k) {
+    static void safe(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {x,y});
-        map[x][y] = 0;
+        queue.offer(new int[] {x,y} );
         visited[x][y] = true;
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int[] now = queue.poll();
             for(int i=0; i<4; i++) {
                 int nx = now[0] + dx[i];
-                int ny = now[0] + dy[i];
-                if(nx>=0 && nx<N && ny>=0 && ny<N && map[nx][ny]<=k && !visited[nx][ny]) {
-                    map[nx][ny] = 0;
-                    visited[nx][ny] = true;
-                    queue.offer(new int[] {nx,ny});
+                int ny = now[1] + dy[i];
+                if(nx>=0 && nx<N && ny>=0 && ny<N) {
+                    if(!visited[nx][ny] && map[nx][ny] >= safelevel) {
+                        visited[nx][ny] = true;
+                        safe(nx,ny);
+                    }
                 }
             }
         }
-        countMap(x,y,map);
     }
 
-    static void countMap(int x,int y,int[][] map) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {x,y});
-        
-
-
-        for(int i=0; i<4; i++) {
-
-        }
-
-
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -58,39 +44,41 @@ public class Baek_2486 {
         map = new int[N][N];
         visited = new boolean[N][N];
 
-        for(int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            for(int j=0; j<N; j++) {
-                // 높이는 1이상 100 이하의 정수이다.
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+               map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int k =0;
-
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
+        int k = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 int max = map[i][j];
-                k = Math.max(k,max);
+                k = Math.max(k, max);       // map 에서 최대 값을 구함
             }
         }
 
-        // int maxSafeZone = Integer.MIN_VALUE;
+        for(int t=1; t<=k; t++) {
+            int answer = 0;
+            safelevel = t;
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    visited[i][j] = false;
+                }
+            }
 
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                if(map[i][j]<=k) {
-                    safe(i,j,k);
-                    k--;
-                    if(k==0) {
-                        break;
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    if(!visited[i][j] && map[i][j] >= safelevel) {
+                        answer ++;
+                        safe(i,j);
                     }
                 }
             }
+            maxcount = Math.max(maxcount,answer);
         }
-
-
-
-
+        System.out.println(maxcount);
     }
 }
+
